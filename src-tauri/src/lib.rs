0c7c5b24f646f7327a47a100bd16e2565c622bb0
Clément Eraud <clement.eraud@gmail.com>
2025-03-05 22:47:47 +0100

    refacto: get operations from tauri back-end

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,40 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
+
+use operations::{Array, Expense, Income, Operations};
+use tauri::command;
+
+mod operations;
+
+#[command]
 fn get_balance() -> i32 {
-    500
+    Operations::new().get_balance()
+}
+
+#[command]
+fn get_incomes() -> Array<Income> {
+    Operations::new().get_incomes().clone()
+}
+
+#[command]
+fn get_planned_expenses() -> Array<Expense> {
+    Operations::new().get_planned_expenses()
+}
+
+#[command]
+fn get_actual_expenses() -> Array<Expense> {
+    Operations::new().get_actual_expenses()
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_balance])
+        .invoke_handler(tauri::generate_handler![
+            get_balance,
+            get_incomes,
+            get_planned_expenses,
+            get_actual_expenses
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
