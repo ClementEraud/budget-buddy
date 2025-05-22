26aa56b97561eb56a593e7ffe050dc37f2356198
Clément Eraud <clement.eraud@gmail.com>
2025-05-23 00:08:09 +0200

    feat: add current month in header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,24 @@
-<h1>Budget Buddy</h1>
+<script lang="ts">
+    import moment from "moment";
+
+    const monthToDisplay = moment().format("MMMM YYYY");
+</script>
+
+<div class="top-header">
+    <h1>Budget Buddy</h1>
+    <div></div>
+    <h2>{monthToDisplay}</h2>
+</div>
 
 <style>
-    h1 {
-        text-align: left;
+    .top-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+
+        h1 {
+            text-align: left;
+        }
     }
 </style>
